31f18423d040420ec2bebd6612ace44cbc0ba779
packetscaper <packetscaper@gmail.com>
2016-10-27 09:30:14 +0000

    added function

--- sshclient.py
+++ sshclient.py
@@ -2,11 +2,14 @@ import paramiko
 import sys 
 
 
-host = sys.argv[1]
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host,username="packetscaper",password="packetscaper")
+def get_ssh_client(self):
 
-print "connected to %s" % host
+ host = sys.argv[1]
+
+ ssh = paramiko.SSHClient()
+ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ ssh.connect(host,username="packetscaper",password="packetscaper")
+
+ return ssh
 
